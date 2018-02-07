d785b3767aacb700b0a01764f5a8958fb48fdbf5
Kris <kristapsrss@gmail.com>
2018-02-07 18:38:20 +0200

    Add routing and default state for home page

--- app/app.js
+++ app/app.js
@@ -1,6 +1,26 @@
 (function(){
 "use strict";
-var cryptoBase = angular.module("CryptoBase", []);
+var cryptoBase = angular.module("CryptoBase", ["ui.router"]);
+
+
+
+cryptoBase.config(["$stateProvider","$urlRouterProvider",
+	function($stateProvider, $urlRouterProvider){
+
+		$urlRouterProvider.otherwise("/");
+
+
+		$stateProvider
+		.state("home",{
+			url: "/",
+			templateUrl: "app/currencyView.html",
+			controller : "CurrencyView as vm"
+		})
+
+	}]
+);
+
+
 
 
 }());
